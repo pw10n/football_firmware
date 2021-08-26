4a0ed0caec113e5099e06f96cdbbe286324c9568
prentice.wongvibulsi <prentice.wongvibulsi@workday.com>
2021-08-26 14:58:00 -0700

    switch to wifi1010

--- football_api.ino
+++ football_api.ino
@@ -1,41 +1,42 @@
 
-#include <Arduino_ConnectionHandler.h>
 #include "secrets.h"
+#include <SPI.h>
+//#include <WiFi101.h>
 
-// set WIFI_SSID and WIFI_PASS in secrets.h
-WiFiConnectionHandler conMan(WIFI_SSID, WIFI_PASS);
+int status = WL_IDLE_STATUS;
+
 WiFiServer server(80);
 
-void setup(){
+void setup() {
+  // initialize serial:
   Serial.begin(9600);
-  while(!Serial) { }
+  Serial.println("Attempting to connect to WPA network...");
+  Serial.print("SSID: ");
+  Serial.println(ssid);
 
-  setDebugMessageLevel(DBG_INFO);
+  status = WiFi.begin(WIFI_SSID, WIFI_PASS);
+  if ( status != WL_CONNECTED) {
+    Serial.println("Couldn't get a wifi connection");
+    while(true);
+  }
+  else {
+    server.begin();
+    Serial.print("Connected to wifi. My address:");
+    IPAddress myAddress = WiFi.localIP();
+    Serial.println(myAddress);
 
-  conMan.addCallback(NetworkConnectionEvent::CONNECTED, onNetworkConnect);
-  conMan.addCallback(NetworkConnectionEvent::DISCONNECTED, onNetworkDisconnect);
-  conMan.addCallback(NetworkConnectionEvent::ERROR, onNetworkError);
+  }
 }
 
 void loop() {
-  /* The following code keeps on running connection workflows on our
-   * ConnectionHandler object, hence allowing reconnection in case of failure
-   * and notification of connect/disconnect event if enabled (see
-   * addConnectCallback/addDisconnectCallback) NOTE: any use of delay() within
-   * the loop or methods called from it will delay the execution of .check(),
-   * which might not guarantee the correct functioning of the ConnectionHandler
-   * object.
-   */
-  conMan.check();
-}
-/* ... */
-void onNetworkConnect() {
-  Serial.println(">>>> CONNECTED to network");
   server.begin();
   Serial.println(">>>> starting server...");
+  while (true){
   WiFiClient client = server.available();
   bool currentLineIsBlank = true;
-  while (client.connected()) {
+    while (client.connected()) {
+
+      Serial.println(">>>> client connected...");
 
       if (client.available()) {
 
@@ -73,12 +74,5 @@ void onNetworkConnect() {
       }
 
     }
-}
-
-void onNetworkDisconnect() {
-  Serial.println(">>>> DISCONNECTED from network");
-}
-
-void onNetworkError() {
-  Serial.println(">>>> ERROR");
+  }
 }
